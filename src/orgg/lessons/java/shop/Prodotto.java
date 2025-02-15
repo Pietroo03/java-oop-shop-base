611a654df81ab5258ccb31c8e6d1e5af9a7462be
Pietroo03 <pietro.ponte.03@gmail.com>
2025-02-15 15:51:28 +0100

    add constructor and methods to class Prodotto

--- src/orgg/lessons/java/shop/Prodotto.java
+++ src/orgg/lessons/java/shop/Prodotto.java
@@ -6,5 +6,24 @@ public class Prodotto {
     public String nome;
     public String descrizione;
     public float prezzo;
-    public String iva;
+    public byte iva;
+
+    public Prodotto(String nome, String descrizione, float prezzo, byte iva) {
+        this.codice = (int) (Math.random() * 100);
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    public void prezzoBase(float prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public float prezzoConIva(float prezzo, byte iva){
+        this.prezzo = prezzo;
+        this.iva = iva;
+        float prezzoConIva = prezzo + (prezzo / 100 * iva );
+        return prezzoConIva;
+    }
 }
