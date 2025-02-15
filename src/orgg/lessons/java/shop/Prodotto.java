16e8ed62d9a7cbff7347aee55bbb6488b2db584a
Pietroo03 <pietro.ponte.03@gmail.com>
2025-02-15 15:23:11 +0100

    add attributes to class Prodotto

--- src/orgg/lessons/java/shop/Prodotto.java
+++ src/orgg/lessons/java/shop/Prodotto.java
@@ -2,4 +2,9 @@ package orgg.lessons.java.shop;
 
 public class Prodotto {
 
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public float prezzo;
+    public String iva;
 }
