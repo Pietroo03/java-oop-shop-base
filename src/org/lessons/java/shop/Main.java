6f66b3a288a97dbdbc10237d48a2f278cb10a2e2
Pietroo03 <pietro.ponte.03@gmail.com>
2025-02-17 16:13:23 +0100

    add static attribute Add new costructor

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -4,13 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         
-        Prodotto penna = new Prodotto("Super Penna", "Facile da usare e infinita!", 4.99f, 22);
+        Prodotto penna = new Prodotto("Super Penna", "Facile da usare e infinita!", 4.99f);
         
-        Prodotto gomma = new Prodotto("Gomma doppia faccia", "Gomma da cancellare doppia faccia, cancelli penna e matita!", 1.50f, 22);
+        Prodotto gomma = new Prodotto("Gomma doppia faccia", "Gomma da cancellare doppia faccia, cancelli penna e matita!", 1.50f);
+
+        Prodotto calcolatrice = new Prodotto();
 
         
         System.out.println(penna.getPrezzoConIva());
         System.out.println(gomma.getNomeEsteso());
+        System.out.println(Prodotto.getIva());
+        calcolatrice.setDescrizione("Utilissimaaa!");
+        System.out.println(calcolatrice.getDescrizione());
     }
 
     
